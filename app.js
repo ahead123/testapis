9a5a4b65c848095ff452c079efbd9dcfa6f43fd2
Justin Rogers <yaskyj@yahoo.com>
2015-01-25 17:54:36 -0600

    Add initial modules and start app.js and index.html

---
+++ app.js
@@ -0,0 +1,13 @@
+"http://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1&origin=http://example.com"
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '390',
+    width: '640',
+    videoId: 'M7lc1UVf-VE',
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
+  });
+}
